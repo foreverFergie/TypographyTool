f12bd2e3edaa5c6cfdc8a5dd3ea7b0f874145308
Erinn <erinnf44@gmail.com>
2020-08-07 19:26:48 -0400

    app stuff

--- getCharacters.m
+++ getCharacters.m
@@ -1,50 +1,54 @@
 
-test=imread('testCases/test_saltAndPepper.png');
-imshow(test);
+%test=imread('testCases/test_saltAndPepper.png');
+%imshow(test);
 
-getChars(test);
+%getChars(test);
+classdef getCharacters
+        
+    methods(Static)
+        function getChars(preProcImg)
 
-function getChars(preProcImg)
-    
-    chars=['A' 'a' 'B' 'b' 'C' 'c' 'D' 'd' 'E' 'e' 'F' 'f' 'G' 'g' 'H' 'I' 'i' 'J' 'j' 'K' 'k' 'L' 'l' 'M' 'm' 'N' 'n' 'O' 'o' 'P' 'p' 'Q' 'q' 'R' 'r' 'S' 's' 'T' 't' 'U' 'u' 'V' 'v' 'W' 'w' 'X' 'x' 'Y' 'y' 'Z' 'z'];
-    ocrTest=ocr(preProcImg);
-    readTest=ocrTest.Text;
-    
-    disp(readTest);
-    
-    for i=1:length(chars)
-        disp(i);
-        if contains(readTest,chars(i))
-            loc=locateText(ocrTest,chars(i));
-            sizeLoc=size(loc);
-            rows=sizeLoc(1);
+            chars=['A' 'a' 'B' 'b' 'C' 'c' 'D' 'd' 'E' 'e' 'F' 'f' 'G' 'g' 'H' 'I' 'i' 'J' 'j' 'K' 'k' 'L' 'l' 'M' 'm' 'N' 'n' 'O' 'o' 'P' 'p' 'Q' 'q' 'R' 'r' 'S' 's' 'T' 't' 'U' 'u' 'V' 'v' 'W' 'w' 'X' 'x' 'Y' 'y' 'Z' 'z'];
+            ocrTest=ocr(preProcImg);
+            readTest=ocrTest.Text;
 
-            x=loc(1);
-            y=loc(rows+1);
-            width=loc((rows*2)+1);
-            height=loc((rows*3)+1);
-            tempLoc=[x y width height];
-            disp(tempLoc);
-            %pImage=preProcImg{loc(1):loc(1)+loc(3),loc(2):loc(2)+loc(4)};
+            disp(readTest);
 
-            tempChar=imcrop(preProcImg,tempLoc);
-            figure();
-            imshow(tempChar);
-            
-            if isstrprop(chars(i),'lower')
-                fileName=append('temp/',chars(i),'_l_match.png');
-                imwrite(tempChar,fileName);
-                
-            else
-                fileName=append('temp/',chars(i),'_match.png');
-                imwrite(tempChar,fileName);
-                
+            for i=1:length(chars)
+                disp(i);
+                if contains(readTest,chars(i))
+                    loc=locateText(ocrTest,chars(i));
+                    sizeLoc=size(loc);
+                    rows=sizeLoc(1);
+
+                    x=loc(1);
+                    y=loc(rows+1);
+                    width=loc((rows*2)+1);
+                    height=loc((rows*3)+1);
+                    tempLoc=[x y width height];
+                    disp(tempLoc);
+                    %pImage=preProcImg{loc(1):loc(1)+loc(3),loc(2):loc(2)+loc(4)};
+
+                    tempChar=imcrop(preProcImg,tempLoc);
+                    figure();
+                    imshow(tempChar);
+
+                    if isstrprop(chars(i),'lower')
+                        fileName=append('temp/',chars(i),'_l_match.png');
+                        imwrite(tempChar,fileName);
+
+                    else
+                        fileName=append('temp/',chars(i),'_match.png');
+                        imwrite(tempChar,fileName);
+
+                    end
+
+
+
+                end
+
             end
-            
-            
-            
+
         end
-
     end
-
 end
--- matchFont.m
+++ matchFont.m
@@ -1,352 +1,371 @@
 
-%try matching algorithm of sorts
-fontMatch=findMatchFont();
+classdef matchFont
 
-function font=findMatchFont()
+    methods(Static)
+        function fonts=findMatchFont()
 
-    charsDir=dir('temp/*.png');
-    
-    %disp(length(charsDir));
-    
-    match=[0 0 0 0 0 0];
-    
-    for i=1:length(charsDir)
-       
-        fileName=charsDir(i).name;
-        %disp(fileName);
-        
-        charVal=fileName(1);
-    
-        full=append('temp/',fileName);
-        img=imread(full);
-        match(1)=match(1)+checkCooper(img,charVal);
-        match(2)=match(2)+checkCourier(img,charVal);
-        match(3)=match(3)+checkHelvetica(img,charVal);
-        match(4)=match(4)+checkRoboto(img,charVal);
-        match(5)=match(5)+checkSaltAndPepper(img,charVal);
-        match(6)=match(6)+checkTimesNewRoman(img,charVal);
-        
-        delete(full);
-        
-    end
-    match=match/length(charsDir);
-    best=max(match);
-    disp(best);
-    
-    locBest=find(match==best);
-    disp(locBest);
+            charsDir=dir('temp/*.png');
 
-    switch (locBest)
-        case 1
-            font='Cooper Black';
-        case 2
-            font='Courier';
-        case 3
-            font='Helvetica';
-        case 4
-            font ='Roboto';
-        case 5
-            font='Salt & Pepper';
-        case 6
-            font='Times New Roman';
-    end
-    
-    
-end
+            %disp(length(charsDir));
 
+            match=[0 0 0 0 0 0];
 
-function percMatch=checkCooper(img,char)
+            for i=1:length(charsDir)
 
-    %
-    %matchImg=rgb2gray(matchImg);
-    ex=imread('Fonts/Cooper Black/example.png');
-    test=ocr(ex);
-    
-    charfind=locateText(test,char);
-    
-    
-    numChars=size(charfind);
-    if numChars(1)~=1
-        matchImgPath=append('Fonts/Cooper Black/',char,'.png');
-        matchImg=imread(matchImgPath);
-        
-    else
-        crop=imcrop(ex,charfind);
-        
-        matchImg=imresize(crop,[40 40]);
-        
-        
-    end
-   
-    matchImg=rgb2gray(matchImg);
-    
-    %disp(size(img));
-    %disp(size(matchImg));
-    
-    %rough match
-    tempImg=imresize(img,[40 40]);
-    tempImg=rgb2gray(tempImg);
-    
-    diffImg=matchImg-tempImg;
-    %imshow(diffImg);
-    
-    nonMatch=0;
-    
-    for i=1:40
-        for j=1:40
-            if diffImg(i,j)~=0
-                nonMatch=nonMatch+1;
-            end
+                fileName=charsDir(i).name;
+                %disp(fileName);
+
+                charVal=fileName(1);
                 
+                
+                full=append('temp/',fileName);
+                img=imread(full);
+                match(1)=match(1)+checkCooper(img,charVal);
+                match(2)=match(2)+checkCourier(img,charVal);
+                match(3)=match(3)+checkHelvetica(img,charVal);
+                match(4)=match(4)+checkRoboto(img,charVal);
+                match(5)=match(5)+checkSaltAndPepper(img,charVal);
+                match(6)=match(6)+checkTimesNewRoman(img,charVal);
+
+                delete(full);
+
+            end
+            match=match/length(charsDir);
+            
+            
+            fonts=[0 0 0 0 0 0];
+            
+            for i=1:3
+                
+                best=max(match);
+                disp(best);
+
+                locBest=find(match==best);
+                disp(locBest);
+                
+                switch (locBest)
+                    case 1
+                        font='Cooper Black';
+                    case 2
+                        font='Courier';
+                    case 3
+                        font='Helvetica';
+                    case 4
+                        font ='Roboto';
+                    case 5
+                        font='Salt & Pepper';
+                    case 6
+                        font='Times New Roman';
+                end
+                
+                nameInd=i*2;
+                matchInd=(i*2)+1;
+                fonts(nameInd)=font;
+                fonts(matchInd)=best;
+                
+                match(locBest)=-1;
+               
+            end
+
+
+        end
+
+
+        function percMatch=checkCooper(img,charCheck)
+
+            %
+            %matchImg=rgb2gray(matchImg);
+            ex=imread('Fonts/Cooper Black/example.png');
+            test=ocr(ex);
+            
+            disp(charCheck);
+            charfind=locateText(test,charCheck);
+
+
+            numChars=size(charfind);
+            if numChars(1)~=1
+                matchImgPath=append('Fonts/Cooper Black/',charCheck,'.png');
+                matchImg=imread(matchImgPath);
+
+            else
+                crop=imcrop(ex,charfind);
+
+                matchImg=imresize(crop,[40 40]);
+
+
+            end
+
+            matchImg=rgb2gray(matchImg);
+
+            %disp(size(img));
+            %disp(size(matchImg));
+
+            %rough match
+            tempImg=imresize(img,[40 40]);
+            tempImg=rgb2gray(tempImg);
+
+            diffImg=matchImg-tempImg;
+            %imshow(diffImg);
+
+            nonMatch=0;
+
+            for i=1:40
+                for j=1:40
+                    if diffImg(i,j)~=0
+                        nonMatch=nonMatch+1;
+                    end
+
+                end
+            end
+
+            percMatch=1-(nonMatch/(40*40));
+            disp(percMatch);
+
+        end
+        function percMatch=checkCourier(img,char)
+
+            %matchImgPath=append('Fonts/Courier/',char,'.png');
+            %matchImg=imread(matchImgPath);
+            %matchImg=rgb2gray(matchImg);
+
+            ex=imread('Fonts/Courier/example.png');
+            test=ocr(ex);
+
+            charfind=locateText(test,char);
+            numChars=size(charfind);
+            if numChars(1)~=1
+                matchImgPath=append('Fonts/Courier/',char,'.png');
+                matchImg=imread(matchImgPath);
+
+            else
+                crop=imcrop(ex,charfind);
+
+                matchImg=imresize(crop,[40 40]);
+
+
+            end
+
+            matchImg=rgb2gray(matchImg);
+            %disp(size(img));
+            %disp(size(matchImg));
+
+            %rough match
+            tempImg=imresize(img,[40 40]);
+            tempImg=rgb2gray(tempImg);
+
+            diffImg=matchImg-tempImg;
+            %imshow(diffImg);
+
+            nonMatch=0;
+
+            for i=1:40
+                for j=1:40
+                    if diffImg(i,j)~=0
+                        nonMatch=nonMatch+1;
+                    end
+
+                end
+            end
+
+            percMatch=1-(nonMatch/(40*40));
+            disp(percMatch);
+
+        end
+        function percMatch=checkHelvetica(img,char)
+
+           % matchImgPath=append('Fonts/Helvetica/',char,'.png');
+            %matchImg=imread(matchImgPath);
+            %matchImg=rgb2gray(matchImg);
+            ex=imread('Fonts/Helvetica/example.png');
+            test=ocr(ex);
+
+            charfind=locateText(test,char);
+            numChars=size(charfind);
+            if numChars(1)~=1
+                matchImgPath=append('Fonts/Helvetica/',char,'.png');
+                matchImg=imread(matchImgPath);
+
+            else
+                crop=imcrop(ex,charfind);
+
+                matchImg=imresize(crop,[40 40]);
+
+
+            end
+
+            matchImg=rgb2gray(matchImg);
+            %disp(size(img));
+            %disp(size(matchImg));
+
+            %rough match
+            tempImg=imresize(img,[40 40]);
+            tempImg=rgb2gray(tempImg);
+
+            diffImg=matchImg-tempImg;
+            %imshow(diffImg);
+
+            nonMatch=0;
+
+            for i=1:40
+                for j=1:40
+                    if diffImg(i,j)~=0
+                        nonMatch=nonMatch+1;
+                    end
+
+                end
+            end
+
+            percMatch=1-(nonMatch/(40*40));
+            disp(percMatch);
+
+        end
+        function percMatch=checkRoboto(img,char)
+
+            %matchImgPath=append('Fonts/Roboto/',char,'.png');
+            %matchImg=imread(matchImgPath);
+            %matchImg=rgb2gray(matchImg);
+
+            ex=imread('Fonts/Roboto/example.png');
+            test=ocr(ex);
+
+            charfind=locateText(test,char);
+            numChars=size(charfind);
+            if numChars(1)~=1
+                matchImgPath=append('Fonts/Roboto/',char,'.png');
+                matchImg=imread(matchImgPath);
+
+            else
+                crop=imcrop(ex,charfind);
+
+                matchImg=imresize(crop,[40 40]);
+
+
+            end
+
+            matchImg=rgb2gray(matchImg);
+
+            %disp(size(img));
+            %disp(size(matchImg));
+
+            %rough match
+            tempImg=imresize(img,[40 40]);
+            tempImg=rgb2gray(tempImg);
+
+            diffImg=matchImg-tempImg;
+            %imshow(diffImg);
+
+            nonMatch=0;
+
+            for i=1:40
+                for j=1:40
+                    if diffImg(i,j)~=0
+                        nonMatch=nonMatch+1;
+                    end
+
+                end
+            end
+
+            percMatch=1-(nonMatch/(40*40));
+            disp(percMatch);
+
+        end
+        function percMatch=checkSaltAndPepper(img,char)
+
+
+            ex=imread('Fonts/Salt & Pepper/example.png');
+            test=ocr(ex);
+
+            charfind=locateText(test,char);
+
+            numChars=size(charfind);
+            if numChars(1)~=1
+                matchImgPath=append('Fonts/Salt & Pepper/',char,'.png');
+                matchImg=imread(matchImgPath);
+
+            else
+                crop=imcrop(ex,charfind);
+
+                matchImg=imresize(crop,[40 40]);
+
+
+            end
+
+            matchImg=rgb2gray(matchImg);
+
+            %disp(size(img));
+            %disp(size(matchImg));
+
+            %rough match
+            tempImg=imresize(img,[40 40]);
+            tempImg=rgb2gray(tempImg);
+
+            diffImg=matchImg-tempImg;
+            %imshow(diffImg);
+
+            nonMatch=0;
+
+            for i=1:40
+                for j=1:40
+                    if diffImg(i,j)~=0
+                        nonMatch=nonMatch+1;
+                    end
+
+                end
+            end
+
+            percMatch=1-(nonMatch/(40*40));
+            disp(percMatch);
+
+        end
+        function percMatch=checkTimesNewRoman(img,char)
+
+           % matchImgPath=append('Fonts/Times New Roman/',char,'.png');
+            %matchImg=imread(matchImgPath);
+            %matchImg=rgb2gray(matchImg);
+
+            ex=imread('Fonts/Times New Roman/example.png');
+            test=ocr(ex);
+
+            charfind=locateText(test,char);
+            numChars=size(charfind);
+            if numChars(1)~=1
+                matchImgPath=append('Fonts/Times New Roman/',char,'.png');
+                matchImg=imread(matchImgPath);
+
+            else
+                crop=imcrop(ex,charfind);
+
+                matchImg=imresize(crop,[40 40]);
+
+
+            end
+
+            matchImg=rgb2gray(matchImg);
+
+            %disp(size(img));
+            %disp(size(matchImg));
+
+            %rough match
+            tempImg=imresize(img,[40 40]);
+            tempImg=rgb2gray(tempImg);
+
+            diffImg=matchImg-tempImg;
+            %imshow(diffImg);
+
+            nonMatch=0;
+
+            for i=1:40
+                for j=1:40
+                    if diffImg(i,j)~=0
+                        nonMatch=nonMatch+1;
+                    end
+
+                end
+            end
+
+            percMatch=1-(nonMatch/(40*40));
+            disp(percMatch);
+
         end
     end
-    
-    percMatch=1-(nonMatch/(40*40));
-    disp(percMatch);
-
-end
-function percMatch=checkCourier(img,char)
-
-    %matchImgPath=append('Fonts/Courier/',char,'.png');
-    %matchImg=imread(matchImgPath);
-    %matchImg=rgb2gray(matchImg);
-    
-    ex=imread('Fonts/Courier/example.png');
-    test=ocr(ex);
-    
-    charfind=locateText(test,char);
-    numChars=size(charfind);
-    if numChars(1)~=1
-        matchImgPath=append('Fonts/Courier/',char,'.png');
-        matchImg=imread(matchImgPath);
-        
-    else
-        crop=imcrop(ex,charfind);
-        
-        matchImg=imresize(crop,[40 40]);
-        
-        
-    end
-    
-    matchImg=rgb2gray(matchImg);
-    %disp(size(img));
-    %disp(size(matchImg));
-    
-    %rough match
-    tempImg=imresize(img,[40 40]);
-    tempImg=rgb2gray(tempImg);
-    
-    diffImg=matchImg-tempImg;
-    %imshow(diffImg);
-    
-    nonMatch=0;
-    
-    for i=1:40
-        for j=1:40
-            if diffImg(i,j)~=0
-                nonMatch=nonMatch+1;
-            end
-                
-        end
-    end
-    
-    percMatch=1-(nonMatch/(40*40));
-    disp(percMatch);
-
-end
-function percMatch=checkHelvetica(img,char)
-
-   % matchImgPath=append('Fonts/Helvetica/',char,'.png');
-    %matchImg=imread(matchImgPath);
-    %matchImg=rgb2gray(matchImg);
-    ex=imread('Fonts/Helvetica/example.png');
-    test=ocr(ex);
-    
-    charfind=locateText(test,char);
-    numChars=size(charfind);
-    if numChars(1)~=1
-        matchImgPath=append('Fonts/Helvetica/',char,'.png');
-        matchImg=imread(matchImgPath);
-        
-    else
-        crop=imcrop(ex,charfind);
-        
-        matchImg=imresize(crop,[40 40]);
-        
-        
-    end
-    
-    matchImg=rgb2gray(matchImg);
-    %disp(size(img));
-    %disp(size(matchImg));
-    
-    %rough match
-    tempImg=imresize(img,[40 40]);
-    tempImg=rgb2gray(tempImg);
-    
-    diffImg=matchImg-tempImg;
-    %imshow(diffImg);
-    
-    nonMatch=0;
-    
-    for i=1:40
-        for j=1:40
-            if diffImg(i,j)~=0
-                nonMatch=nonMatch+1;
-            end
-                
-        end
-    end
-    
-    percMatch=1-(nonMatch/(40*40));
-    disp(percMatch);
-
-end
-function percMatch=checkRoboto(img,char)
-
-    %matchImgPath=append('Fonts/Roboto/',char,'.png');
-    %matchImg=imread(matchImgPath);
-    %matchImg=rgb2gray(matchImg);
-    
-    ex=imread('Fonts/Roboto/example.png');
-    test=ocr(ex);
-    
-    charfind=locateText(test,char);
-    numChars=size(charfind);
-    if numChars(1)~=1
-        matchImgPath=append('Fonts/Roboto/',char,'.png');
-        matchImg=imread(matchImgPath);
-        
-    else
-        crop=imcrop(ex,charfind);
-        
-        matchImg=imresize(crop,[40 40]);
-        
-        
-    end
-    
-    matchImg=rgb2gray(matchImg);
-    
-    %disp(size(img));
-    %disp(size(matchImg));
-    
-    %rough match
-    tempImg=imresize(img,[40 40]);
-    tempImg=rgb2gray(tempImg);
-    
-    diffImg=matchImg-tempImg;
-    %imshow(diffImg);
-    
-    nonMatch=0;
-    
-    for i=1:40
-        for j=1:40
-            if diffImg(i,j)~=0
-                nonMatch=nonMatch+1;
-            end
-                
-        end
-    end
-    
-    percMatch=1-(nonMatch/(40*40));
-    disp(percMatch);
-
-end
-function percMatch=checkSaltAndPepper(img,char)
-
-   
-    ex=imread('Fonts/Salt & Pepper/example.png');
-    test=ocr(ex);
-    
-    charfind=locateText(test,char);
-    
-    numChars=size(charfind);
-    if numChars(1)~=1
-        matchImgPath=append('Fonts/Salt & Pepper/',char,'.png');
-        matchImg=imread(matchImgPath);
-        
-    else
-        crop=imcrop(ex,charfind);
-        
-        matchImg=imresize(crop,[40 40]);
-        
-        
-    end
-    
-    matchImg=rgb2gray(matchImg);
-    
-    %disp(size(img));
-    %disp(size(matchImg));
-    
-    %rough match
-    tempImg=imresize(img,[40 40]);
-    tempImg=rgb2gray(tempImg);
-    
-    diffImg=matchImg-tempImg;
-    %imshow(diffImg);
-    
-    nonMatch=0;
-    
-    for i=1:40
-        for j=1:40
-            if diffImg(i,j)~=0
-                nonMatch=nonMatch+1;
-            end
-                
-        end
-    end
-    
-    percMatch=1-(nonMatch/(40*40));
-    disp(percMatch);
-
-end
-function percMatch=checkTimesNewRoman(img,char)
-
-   % matchImgPath=append('Fonts/Times New Roman/',char,'.png');
-    %matchImg=imread(matchImgPath);
-    %matchImg=rgb2gray(matchImg);
-    
-    ex=imread('Fonts/Times New Roman/example.png');
-    test=ocr(ex);
-    
-    charfind=locateText(test,char);
-    numChars=size(charfind);
-    if numChars(1)~=1
-        matchImgPath=append('Fonts/Times New Roman/',char,'.png');
-        matchImg=imread(matchImgPath);
-        
-    else
-        crop=imcrop(ex,charfind);
-        
-        matchImg=imresize(crop,[40 40]);
-        
-        
-    end
-    
-    matchImg=rgb2gray(matchImg);
-    
-    %disp(size(img));
-    %disp(size(matchImg));
-    
-    %rough match
-    tempImg=imresize(img,[40 40]);
-    tempImg=rgb2gray(tempImg);
-    
-    diffImg=matchImg-tempImg;
-    %imshow(diffImg);
-    
-    nonMatch=0;
-    
-    for i=1:40
-        for j=1:40
-            if diffImg(i,j)~=0
-                nonMatch=nonMatch+1;
-            end
-                
-        end
-    end
-    
-    percMatch=1-(nonMatch/(40*40));
-    disp(percMatch);
-
 end
